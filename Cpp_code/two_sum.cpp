f2a124f3a93466bff4e438163444b206da5868d5
ChickenBenny <zxc123benny14159@gmail.com>
2022-06-23 16:11:50 +0800

    Two sum code

---
+++ Cpp_code/two_sum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void two_sum(vector<int>& nums, int target) {
+    vector<int> answer;
+    for(int i = 0;i < nums.size() - 1;i ++){
+        for(int j = i + 1;j < nums.size();j ++){
+            if(nums[i] + nums[j] == target){
+                answer.push_back(i);
+                answer.push_back(j);
+                cout << i << "  "<< j << endl; 
+                return;
+            }
+        }
+    }
+    
+}
+
+int main(){
+    vector<int> v1 = {2, 7, 11, 15}, v2 = {3, 2, 4}, v3 = {3, 3};
+    int t1 = 9, t2 = 6, t3 = 6;
+    two_sum(v1, t1);
+    two_sum(v2, t2);
+    two_sum(v3, t3);
+}
