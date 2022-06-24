5664556c4348c1830514c39cde0a64f21a7b51db
ChickenBenny <zxc123benny14159@gmail.com>
2022-06-24 17:58:18 +0800

    delete code

--- two_sum.cpp
+++ two_sum.cpp
@@ -3,19 +3,18 @@
 
 using namespace std;
 
-void two_sum(vector<int>& nums, int target) {
+vector<int> two_sum(vector<int>& nums, int target) {
     vector<int> answer;
     for(int i = 0;i < nums.size() - 1;i ++){
         for(int j = i + 1;j < nums.size();j ++){
             if(nums[i] + nums[j] == target){
                 answer.push_back(i);
                 answer.push_back(j);
-                cout << i << "  "<< j << endl; 
-                return;
+                return answer;
             }
         }
     }
-    
+    return{0, 0};
 }
 
 int main(){
